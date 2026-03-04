e6582871b3b22ec647c42911da4800af1de8288a
guizinteixeira19-sudo <guizinteixeira19@gmail.com>
2026-03-04 18:00:21 -0300

    Fix background image URL syntax in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: 'url("estetica-do-sorriso-para-eventos-especiais-planejamento-estrategico-para-um-sorriso-perfeito")',
+          backgroundImage: "url("estetica-do-sorriso-para-eventos-especiais-planejamento-estrategico-para-um-sorriso-perfeito")",
         }}
       />
 
